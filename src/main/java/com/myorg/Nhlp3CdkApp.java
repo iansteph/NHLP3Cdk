3f8e9259ea18c37abe2bd01365c60c56804d2074
Ian Stephenson 🦈🏒 <ianp.stephenson@yahoo.com>
2020-06-23 23:30:48 -0700

    Initial CDK commit

---
+++ src/main/java/com/myorg/Nhlp3CdkApp.java
@@ -0,0 +1,15 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.App;
+
+import java.util.Arrays;
+
+public class Nhlp3CdkApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        new Nhlp3CdkStack(app, "Nhlp3CdkStack");
+
+        app.synth();
+    }
+}
