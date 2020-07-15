7aff2416d468dad30e1d934556ce08a313bae7bb
Ian Stephenson 🦈🏒 <ianp.stephenson@yahoo.com>
2020-07-14 23:30:52 -0700

    Create audit SQS queue
    - Queue will receive all the events published from the SNS topic for various purposes

--- src/main/java/iansteph/nhlp3/cdk/stack/ShiftPublisherStack.java
+++ src/main/java/iansteph/nhlp3/cdk/stack/ShiftPublisherStack.java
@@ -7,11 +7,22 @@ import software.amazon.awscdk.core.StackProps;
 import software.amazon.awscdk.services.dynamodb.Attribute;
 import software.amazon.awscdk.services.dynamodb.AttributeType;
 import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.iam.Effect;
+import software.amazon.awscdk.services.iam.PolicyStatement;
+import software.amazon.awscdk.services.iam.ServicePrincipal;
 import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.sns.Subscription;
+import software.amazon.awscdk.services.sns.SubscriptionProtocol;
 import software.amazon.awscdk.services.sns.Topic;
+import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.sqs.QueuePolicy;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShiftPublisherStack extends Stack {
 
@@ -27,6 +38,33 @@ public class ShiftPublisherStack extends Stack {
         // SNS topic for publishing shift events
         final Topic shiftPublishingShiftTopic = Topic.Builder.create(this, "ShiftPublisherSnsTopic")
                 .topicName("NHLP3-Shift-Events")
+                .build();
+
+        // Audit queue-- queue to be used for sanity checking, sns message publishing, serialization, and various other things
+        final Queue shiftPublishingAuditQueue = Queue.Builder.create(this, "ShiftPublishingAuditQueue")
+                .queueName("NHLP3-ShiftPublishing-Audit")
+                .retentionPeriod(Duration.days(14))
+                .build();
+        final QueuePolicy queuePolicy = QueuePolicy.Builder.create(this, "ShiftPublishingAuditQueuePolicy")
+                .queues(Collections.singletonList(shiftPublishingAuditQueue))
+                .build();
+        final Map<String, Object> conditions = new HashMap<>();
+        final Map<String, String> conditionDetail = new HashMap<>();
+        conditionDetail.put("aws:SourceArn", shiftPublishingShiftTopic.getTopicArn());
+        conditions.put("ArnEquals", conditionDetail);
+        final PolicyStatement shiftPublishingAuditQueuePolicyStatement = PolicyStatement.Builder.create()
+                .effect(Effect.ALLOW)
+                .principals(Collections.singletonList(ServicePrincipal.Builder.create("sns.amazonaws.com").build()))
+                .actions(Collections.singletonList("sqs:SendMessage"))
+                .resources(Collections.singletonList(shiftPublishingAuditQueue.getQueueArn()))
+                .conditions(conditions)
+                .build();
+        queuePolicy.getDocument().addStatements(shiftPublishingAuditQueuePolicyStatement);
+        Subscription.Builder.create(this, "AuditQueueToShiftPublisherSnsTopicSubscription")
+                .topic(shiftPublishingShiftTopic)
+                .protocol(SubscriptionProtocol.SQS)
+                .endpoint(shiftPublishingAuditQueue.getQueueArn())
+                .rawMessageDelivery(true)
                 .build();
 
         // S3 bucket to store the packaged artifacts for the ShiftPublisher lambda function
